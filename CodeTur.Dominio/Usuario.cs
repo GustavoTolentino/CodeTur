b3a364b854d6c66c3495d21a4c07fa581b71beb8
Gustavo Tolentino <tolentinogustavo@gmail.com>
2021-09-01 10:09:25 -0300

    Projeto codeTur

---
+++ CodeTur.Dominio/Usuario.cs
@@ -0,0 +1,36 @@
+﻿using CodeTur.Comum;
+using Flunt.Notifications;
+using Flunt.Validations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeTur.Dominio
+{
+    public class Usuario : Base
+    {
+        public Usuario(string nome, string email, string senha, EnTipoUsuario tipoUsuario)
+        {
+
+            AddNotifications(
+            new Contract<Notification>()
+                 .Requires()
+                 .IsNotEmpty(nome, "Nome", "Nome não pode ser vazio")
+                 .IsEmail(email, "Email", "O formato do Email nao esta correto")
+                 .IsGreaterThan(senha, 7, "Senha", "A senha deve possuir no minimo 8 caracteres")
+            );
+
+            Nome = nome;
+            Email = email;
+            Senha = senha;
+            TipoUsuario = tipoUsuario;
+        }
+
+        public string Nome { get; private set; }
+        public string Email { get; private set; }
+        public string Senha { get; private set; }
+        public EnTipoUsuario TipoUsuario { get; private set; }
+    }
+}
---
+++ CodeTur.Testes/UsuarioTestes.cs
@@ -0,0 +1,26 @@
+﻿using CodeTur.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CodeTur.Testes
+{
+    public class UsuarioTestes
+    {
+        [Fact]
+        public void DeveRetornarSeUsuarioForValido()
+        {
+            Usuario user = new Usuario(
+                "Gustavo",
+                "tolentinogustavo@gmail.com",
+                "123456789",
+                EnTipoUsuario.Admin
+                );
+
+            Assert.True(user.IsValid, "Valid User");
+        }
+    }
+}
